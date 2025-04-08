07258dcd7a08a647a8a6ab19a013ead528d39bb5
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2025-04-08 11:28:24 +0000

    compute latent points

--- data_creator.py
+++ data_creator.py
@@ -9,7 +9,121 @@ class DataCreator:
     def __init__(self, data_path: str):
         self.data_path = data_path
         
-    def create(self, map_z_to_y=True):
+    def _map_mesh_z_to_y(self, mesh: trimesh.Trimesh) -> trimesh.Trimesh:
+
+        mapped_mesh = mesh.copy()
+        mapped_mesh.vertices[:, [1, 2]] = mapped_mesh.vertices[:, [2, 1]]
+        
+        return mapped_mesh
+        
+    def _centralize_mesh(self, mesh: trimesh.Trimesh) -> trimesh.Trimesh:
+        
+        centralized_mesh = mesh.copy()
+        
+        centralized_mesh.vertices -= centralized_mesh.centroid
+        assert np.allclose(centralized_mesh.centroid, 0)
+        
+        return centralized_mesh
+        
+    def _orient_mesh(self, mesh: trimesh.Trimesh) -> trimesh.Trimesh:
+        
+        oriented_mesh = mesh.copy()
+        
+        # compute obb information
+        transform, (w, h) = trimesh.bounds.oriented_bounds_2D(oriented_mesh.vertices[:, :2])
+        
+        # define the 2d bounding box from the above information
+        bounding_box_2d = np.array([
+            [-w / 2, -h / 2],
+            [w / 2, -h / 2],
+            [w / 2, h / 2],
+            [-w / 2, h / 2]
+        ])
+        
+        # apply the transformation to get the oriented bounding box
+        oriented_bounding_box_2d = bounding_box_2d @ transform[:2, :2]
+        
+        # sort the obb vertices by angle
+        center = np.mean(oriented_bounding_box_2d, axis=0)
+        angles = np.arctan2(
+            oriented_bounding_box_2d[:, 1] - center[1], 
+            oriented_bounding_box_2d[:, 0] - center[0]
+        )
+        sorted_indices = np.argsort(angles)
+        oriented_bounding_box_2d = oriented_bounding_box_2d[sorted_indices]
+        
+        # orient the mesh
+        angle_to_orient = -np.arctan2(*(oriented_bounding_box_2d[2] - oriented_bounding_box_2d[1]))
+        rotation_matrix = np.array(
+            [
+                [np.cos(angle_to_orient), -np.sin(angle_to_orient), 0],
+                [np.sin(angle_to_orient), np.cos(angle_to_orient), 0],
+                [0, 0, 1],
+            ]
+        )
+        
+        oriented_mesh.vertices = oriented_mesh.vertices @ rotation_matrix
+        
+        return oriented_mesh
+    
+    def _compute_latent_points(self, mesh: trimesh.Trimesh) -> np.ndarray:
+            
+        box_mesh = trimesh.creation.box(bounds=mesh.bounds)
+        box_mesh.vertices = box_mesh.vertices @ np.array([[0.95, 0, 0], [0, 0.95, 0], [0, 0, 1]])
+        box_mesh_subdivided = box_mesh.subdivide()
+        
+        (min_x, min_y, min_z), (max_x, max_y, max_z) = box_mesh.bounds
+        
+        nearest_indices = []
+        for i, vertex in enumerate(box_mesh_subdivided.vertices):
+            is_bottom_edge_midpoint = (
+                np.allclose(vertex, (np.array([min_x, min_y, min_z]) + np.array([max_x, min_y, min_z])) * 0.5) 
+                or np.allclose(vertex, (np.array([min_x, max_y, min_z]) + np.array([max_x, max_y, min_z])) * 0.5) 
+                or np.allclose(vertex, (np.array([min_x, min_y, min_z]) + np.array([max_x, max_y, min_z])) * 0.5) 
+                or np.allclose(vertex, (np.array([min_x, min_y, min_z]) + np.array([min_x, max_y, min_z])) * 0.5) 
+                or np.allclose(vertex, (np.array([max_x, min_y, min_z]) + np.array([max_x, max_y, min_z])) * 0.5)
+            )
+            
+            is_top_edge_midpoint = (
+                np.allclose(vertex, (np.array([min_x, min_y, max_z]) + np.array([max_x, min_y, max_z])) * 0.5) 
+                or np.allclose(vertex, (np.array([min_x, max_y, max_z]) + np.array([max_x, max_y, max_z])) * 0.5) 
+                or np.allclose(vertex, (np.array([min_x, min_y, max_z]) + np.array([max_x, max_y, max_z])) * 0.5) 
+                or np.allclose(vertex, (np.array([min_x, min_y, max_z]) + np.array([min_x, max_y, max_z])) * 0.5) 
+                or np.allclose(vertex, (np.array([max_x, min_y, max_z]) + np.array([max_x, max_y, max_z])) * 0.5)
+            )
+            
+            if is_bottom_edge_midpoint or is_top_edge_midpoint:
+                ray_origin = vertex
+                ray_direction = np.array([0, 0, 1])
+                
+                # flip ray_direction
+                if is_top_edge_midpoint:
+                    ray_direction *= -1
+                
+                locations, *_ = mesh.ray.intersects_location(
+                    ray_origins=[ray_origin], 
+                    ray_directions=[ray_direction]
+                )
+                
+                # if the ray intersects the mesh, relocate the vertex to the intersection point
+                if len(locations) > 0:
+                    new_vertex = locations[0]
+                    box_mesh_subdivided.vertices[i] = new_vertex
+                else:
+                    # if not, relocate the vertex to the nearest point on the mesh
+                    nearest_indices.append(i)
+
+            else:
+                nearest_indices.append(i)
+            
+        nearest_vertices = mesh.nearest.on_surface(box_mesh_subdivided.vertices[nearest_indices])[0]
+        box_mesh_subdivided.vertices[nearest_indices] = nearest_vertices
+
+        assert box_mesh_subdivided.vertices.shape == (26, 3)
+
+        return box_mesh_subdivided.vertices
+        
+    def create(self, map_z_to_y: bool = True) -> None:
         grid_size = 192
         min_bound = -1
         max_bound = 1
@@ -19,16 +133,12 @@ class DataCreator:
         xx, yy, zz = np.meshgrid(x, y, z, indexing='ij')
         xyz = np.stack([xx, yy, zz], axis=-1).reshape(-1, 3)
 
-
         class_number = 0
-        while True:
-            break
-
-        count = 0
 
         data_list = sorted(os.listdir(self.data_path))
         for file in data_list:
-            if count == 10:
+
+            if class_number == 10:
                 break
 
             obj_path = os.path.join(self.data_path, file, "models", "model_normalized.obj")
@@ -49,76 +159,30 @@ class DataCreator:
             
             # map z to y
             if map_z_to_y:
-                mesh.vertices[:, [1, 2]] = mesh.vertices[:, [2, 1]]
+                mesh = self._map_mesh_z_to_y(mesh)
             
-            # centralize the mesh
-            mesh.vertices -= mesh.centroid
-            assert np.allclose(mesh.centroid, 0)
+            # centralize the mesh to the origin
+            mesh = self._centralize_mesh(mesh)
             
-            box_mesh = trimesh.creation.box(bounds=mesh.bounds)
-            box_mesh.vertices = box_mesh.vertices @ np.array([[0.95, 0, 0], [0, 0.95, 0], [0, 0, 1]])
-            box_mesh_subdivided = box_mesh.subdivide()
+            # orient the mesh
+            mesh = self._orient_mesh(mesh)
             
-            (min_x, min_y, min_z), (max_x, max_y, max_z) = box_mesh.bounds
-            
-            nearest_indices = []
-            for i, vertex in enumerate(box_mesh_subdivided.vertices):
-                is_bottom_edge_midpoint = (
-                    np.allclose(vertex, (np.array([min_x, min_y, min_z]) + np.array([max_x, min_y, min_z])) * 0.5) 
-                    or np.allclose(vertex, (np.array([min_x, max_y, min_z]) + np.array([max_x, max_y, min_z])) * 0.5) 
-                    or np.allclose(vertex, (np.array([min_x, min_y, min_z]) + np.array([max_x, max_y, min_z])) * 0.5) 
-                    or np.allclose(vertex, (np.array([min_x, min_y, min_z]) + np.array([min_x, max_y, min_z])) * 0.5) 
-                    or np.allclose(vertex, (np.array([max_x, min_y, min_z]) + np.array([max_x, max_y, min_z])) * 0.5)
-                )
-                
-                is_top_edge_midpoint = (
-                    np.allclose(vertex, (np.array([min_x, min_y, max_z]) + np.array([max_x, min_y, max_z])) * 0.5) 
-                    or np.allclose(vertex, (np.array([min_x, max_y, max_z]) + np.array([max_x, max_y, max_z])) * 0.5) 
-                    or np.allclose(vertex, (np.array([min_x, min_y, max_z]) + np.array([max_x, max_y, max_z])) * 0.5) 
-                    or np.allclose(vertex, (np.array([min_x, min_y, max_z]) + np.array([min_x, max_y, max_z])) * 0.5) 
-                    or np.allclose(vertex, (np.array([max_x, min_y, max_z]) + np.array([max_x, max_y, max_z])) * 0.5)
-                )
-                
-                if is_bottom_edge_midpoint or is_top_edge_midpoint:
-                    ray_origin = vertex
-                    ray_direction = np.array([0, 0, 1])
-                    
-                    # flip ray_direction
-                    if is_top_edge_midpoint:
-                        ray_direction *= -1
-                    
-                    locations, *_ = mesh.ray.intersects_location(
-                        ray_origins=[ray_origin], 
-                        ray_directions=[ray_direction]
-                    )
-                    
-                    # if the ray intersects the mesh, relocate the vertex to the intersection point
-                    if len(locations) > 0:
-                        new_vertex = locations[0]
-                        box_mesh_subdivided.vertices[i] = new_vertex
-                    else:
-                        # if not, relocate the vertex to the nearest point on the mesh
-                        nearest_indices.append(i)
-
-                else:
-                    nearest_indices.append(i)
-                
-            nearest_vertices = mesh.nearest.on_surface(box_mesh_subdivided.vertices[nearest_indices])[0]
-            box_mesh_subdivided.vertices[nearest_indices] = nearest_vertices
+            # compute latent points
+            latent_points = self._compute_latent_points(mesh)
 
             # compute sdf values
             sdf, *_ = pcu.signed_distance_to_mesh(xyz, mesh.vertices, mesh.faces)
             
-            # latent vector와 함께 저장될 (x, y, z, sdf) 데이터를 생성
-            # data = np.hstack([xyz, sdf[:, np.newaxis]])
+            data = {
+                "class_number": class_number,
+                "xyz": xyz,
+                "sdf": sdf,
+                "latent_points": latent_points,
+            }
             
+            np.save(f"{class_number}.npy", data)
 
-            # Save both the SDF data and the reconstructed mesh
-            # np.save(f"{count}.npy", data)
-            # reconstructed_mesh.export(f"{count}_reconstructed.obj")
-            # mesh.export(f"{count}_mesh.obj")
-            
-            count += 1
+            class_number += 1
             
     
     
