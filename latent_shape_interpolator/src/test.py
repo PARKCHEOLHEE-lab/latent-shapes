20f9cb50cab169299fceef3aebb1be5eff27ad26
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2025-07-10 12:21:44 +0000

    test.py wip

--- latent_shape_interpolator/src/test.py
+++ latent_shape_interpolator/src/test.py
@@ -1,56 +1,51 @@
+if __name__ == "__main__":
+    # CUDA_VISIBLE_DEVICE=0 python train.py
 
-import os
-import sys
+    import os
+    import sys
 
-# print(f"CUDA_VISIBLE_DEVICES: {os.environ['CUDA_VISIBLE_DEVICES']}")
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    print(f"CUDA_VISIBLE_DEVICES: {os.environ['CUDA_VISIBLE_DEVICES']}")
+    import torch
 
-import torch
+    if os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")) not in sys.path:
+        sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 
+    from latent_shape_interpolator.src.config import Configuration
+    from latent_shape_interpolator.src.data import SDFDataset
+    from latent_shape_interpolator.src.model import SDFDecoder, LatentShapes
+    from latent_shape_interpolator.src.trainer import Trainer
 
-if os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")) not in sys.path:
-    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+    configuration = Configuration()
+    configuration.set_seed()
 
-from latent_shape_interpolator.src.config import Configuration
-from latent_shape_interpolator.src.data import SDFDataset
-from latent_shape_interpolator.src.model import SDFDecoder
-from latent_shape_interpolator.src.trainer import Trainer
+    sdf_dataset = SDFDataset.create_dataset(
+        data_dir=configuration.DATA_PATH_PROCESSED, configuration=configuration, data_slicer=1
+    )
 
-configuration = Configuration()
-configuration.set_seed()
+    latent_shapes = LatentShapes(latent_shapes=sdf_dataset.latent_shapes, noise_min=-0.1, noise_max=0.1)
 
-sdf_dataset = SDFDataset.create_dataset(
-    data_dir=configuration.DATA_PATH_PROCESSED, configuration=configuration, data_slicer=10
-)
+    sdf_decoder = SDFDecoder(configuration=configuration)
 
-sdf_decoder = SDFDecoder(
-    latent_shapes=sdf_dataset.latent_shapes,
-    configuration=configuration,
-)
+    states = torch.load(
+        os.path.abspath(os.path.join(os.path.dirname(__file__), "../runs/07-09-2025__09-44-54/states.pth"))
+    )
 
-sdf_decoder_module = sdf_decoder
+    sdf_decoder.load_state_dict(states["state_dict_model"])
+    latent_shapes.load_state_dict(states["state_dict_latent_shapes"])
 
-if configuration.USE_MULTI_GPUS:
-    sdf_decoder = torch.nn.DataParallel(sdf_decoder, device_ids=[i for i in range(torch.cuda.device_count())])
+    # latent_shapes_batch = sdf_dataset.latent_shapes[
+    #     torch.randperm(sdf_dataset.num_classes)[: configuration.RECONSTRUCTION_COUNT]
+    # ]
 
-    sdf_decoder_module = sdf_decoder.module
+    latent_shapes_batch = latent_shapes(torch.randperm(sdf_dataset.num_classes)[: configuration.RECONSTRUCTION_COUNT])
 
-sdf_decoder_module.load_state_dict(
-    torch.load(
-        os.path.abspath(os.path.join(os.path.dirname(__file__), "../runs/07-03-2025__09-27-28/states.pth"))
-    )["state_dict_model"]
-)
-
-
-latent_shapes_batch = sdf_decoder_module.latent_shapes_embedding(
-    torch.randperm(sdf_dataset.num_classes)[: configuration.RECONSTRUCTION_COUNT]
-)
-
-reconstruction_results = sdf_decoder.reconstruct(
-    sdf_dataset.latent_shapes[0].unsqueeze(0),
-    save_path="",
-    normalize=True,
-    check_watertight=False,
-    add_noise=False,
-    rescale=True,
-)
+    for _ in range(1):
+        reconstruction_results = sdf_decoder.reconstruct(
+            latent_shapes_batch,
+            save_path="",
+            normalize=True,
+            check_watertight=False,
+            add_noise=False,
+            rescale=True,
+        )
